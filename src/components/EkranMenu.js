455460552ecb9dd02b2ca3c94a66d7ceab88acbb
KWN-KSL <kisiel.kewin123@gmail.com>
2026-01-15 13:09:02 +0100

    poprawka w menu

--- src/components/EkranMenu.js
+++ src/components/EkranMenu.js
@@ -9,7 +9,7 @@ export default function EkranMenu({ saldo, graj, resetuj }) {
       <h1>Blackjack</h1>
       <h2>Saldo: ${saldo}</h2>
       <button onClick={graj} className="przycisk-menu">Graj</button>
-      <button onClick={resetuj} className="przycisk-menu reset">Resetuj Grę</button>
+      <button onClick={resetuj} className="przycisk-menu reset">Resetuj punkty</button>
       <button onClick={() => ustawPokazZasady(true)} className="przycisk-menu zasady">Zasady</button>
       {pokazZasady && <ModalZasady zamknij={() => ustawPokazZasady(false)} />}
     </div>
